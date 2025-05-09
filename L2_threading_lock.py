9e104914416988189aa3a01feb10e4aa90e5bec1
yrtej3 <yrtej3@gmail.com>
2025-05-09 18:40:28 +0530

    introducing threading.Condition

--- L2_threading_lock.py
+++ L2_threading_lock.py
@@ -14,30 +14,36 @@ import time
 
 countdown_timer = 10
 lock = threading.Lock()
+condition = threading.Condition(lock)
 
 def decrement():
     global countdown_timer
     while countdown_timer > 0:
-        with lock:
-            print("Lock acquired")
+        with condition:
             countdown_timer -= 1
+            print("Lock acquired by decrement")
+            condition.notify_all()
         time.sleep(1)
 
 def run_check():
     global countdown_timer
-    while countdown_timer > 0:
-        with lock:
-            print(countdown_timer)
-        time.sleep(1)
-
+    previous_value = None
+    while True:
+        with condition:
+            if countdown_timer <= 0:
+                break
+            if countdown_timer != previous_value:
+                print(f'Countdown: {countdown_timer}')
+                previous_value = countdown_timer
+        time.sleep(0.1)
 
 thread1 = threading.Thread(target=decrement)
 thread2 = threading.Thread(target=run_check)
 
-thread1.start()
 thread2.start()
+thread1.start()
 
-thread1.join()
 thread2.join()
+thread1.join()
 
 print("concurrency achieved with threading")
